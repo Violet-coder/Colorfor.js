74fc8ff67909848866307ec31e3fb037fe741713
Violet-coder <violetzzy6@gmail.com>
2020-07-26 10:51:48 -0400

    server modified

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const app = express()
 app.use(express.static(__dirname + '/pub'))
 
 const port = process.env.PORT || 5000
-app.listen(5000, () =>{
+app.listen(port, () =>{
     log(`Listening on port ${port}...`)
 })
