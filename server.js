545378f9ed815d20881ea842ed3ee68769b7e996
Violet-coder <violetzzy6@gmail.com>
2020-08-21 17:50:28 -0400

    update

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 
 app.use(express.static(__dirname + '/pub'))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/examples.html');
+  })
+
 const port = process.env.PORT || 5000
 app.listen(port, () =>{
     log(`Listening on port ${port}...`)
